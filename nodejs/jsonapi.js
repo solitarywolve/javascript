cd09fc58b60b04c99b0bdf3d16416daf167fb691
ly khanh duy <solitarywolve@gmail.com>
2017-10-13 09:44:37 +0700

    auto request /favicon.ico

--- nodejs/jsonapi.js
+++ nodejs/jsonapi.js
@@ -1,17 +1,20 @@
 var http = require("http");
 var url = require("url");
 function parseISOString(s) {
-    if(s !== undefined){
-        var b = s.split(/\D+/);
-        return new Date(Date.UTC(b[0], --b[1], b[2], b[3], b[4], b[5], b[6]));
-    }else return 'a';
-  }
+         var b = s.split(/\D+/);
+         return new Date(Date.UTC(b[0], --b[1], b[2], b[3], b[4], b[5], b[6]));
+   }
 http.createServer(function(req, res) {
 
-  var parsedUrl = url.parse(req.url, true); // true to get query as object
-  var queryAsObject = parsedUrl.query;
-
-  console.log(parseISOString(queryAsObject.iso).toString())
-  res.end(parseISOString(queryAsObject.iso).toString());
+  if (req.url === '/favicon.ico') {
+    res.writeHead(200, {'Content-Type': 'image/x-icon'} );
+    res.end(/* icon content here */);
+  } else {
+    var parsedUrl = url.parse(req.url, true); // true to get query as object
+    var queryAsObject = parsedUrl.query;
+    console.log(queryAsObject.iso)
+    res.end((parseISOString(queryAsObject.iso)).toString());
+  }
+  
 
 }).listen(8080);
